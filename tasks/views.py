0f5330c6702037e9c726e84dd3abeea38013e42b
once27 <117838893+once27@users.noreply.github.com>
2025-04-06 12:27:20 +0530

    added login,logout and authentication

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
-
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import logout
+from .serializers import RegisterSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import AuthenticationFailed
 # Create your views here.
+
+class RegisterAPIView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({
+            "user_id": user.id,
+            "username": user.username,
+            "token": token.key
+        })
+
+
+class LoginAPIView(ObtainAuthToken):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({
+            "user_id": token.user_id,
+            "username": token.user.username,
+            "token": token.key
+        })
+
+class LogoutAPIView(generics.GenericAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            request.user.auth_token.delete()
+        except:
+            raise AuthenticationFailed("Invalid or missing token.")
+        logout(request)
+        return Response({"message": "Logged out successfully"}, status=status.HTTP_200_OK)
+
+# class LogoutAPIView(generics.GenericAPIView):
+#     def post(self, request, *args, **kwargs):
+#         request.user.auth_token.delete()
+#         logout(request)
+#         return Response({"message": "Logged out successfully"}, status=status.HTTP_200_OK)
